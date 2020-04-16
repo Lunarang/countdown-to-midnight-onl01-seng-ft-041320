012b8b001f5345cffdeb27ba472c8ad16f01d25b
Erin Mettille <ecmettille@gmail.com>
2020-04-16 15:25:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown(seconds)
-  while num_of_hotdogs_eaten < 7
-    num_of_hotdogs_eaten += 1
+  while seconds > 0
+    seconds -= 1
     puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
   end
 end
